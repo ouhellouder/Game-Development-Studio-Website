aeacbd98337033fb0905793185826338485ebe0e
ouhellouder <78954248+ouhellouder@users.noreply.github.com>
2025-03-13 22:20:00 +0100

    Add files via upload

--- parts/header.php
+++ parts/header.php
@@ -1,8 +1,10 @@
 <header>
     <section>
+        <div></div>
         <h1>Unknown minute game studio</h1>
+
         <a href="index.html" style="color: rgb(98, 98, 98);">Home Page</a>
-        <a href="webpages/models.html">Models</a>
+        <a href="webpages/cafeteria.html">Cafeteria</a>
         <a href="webpages/games.html">Games</a>
     </section>
 </header>
