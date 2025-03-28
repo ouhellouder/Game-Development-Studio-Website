03dd45bb2870ef0628ad8ec451c59eaefc7d79c9
ouhellouder <78954248+ouhellouder@users.noreply.github.com>
2025-03-28 13:11:51 +0100

    Add files via upload

---
+++ game_info.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<?php include_once "parts/header.html"?>
+
+
+
+<body>
+   <br>
+   <br>
+<?php include_once("parts/gamediv.php");
+
+
+?>
+
+
+
+
+<script src="js/show.js"></script>
+<script src="js/tweaker.js"></script>
+</body>
+<?php include_once("parts/footer.html");?>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Last Minute Game Studio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+</head>
+
+
+<?php include_once "parts/header.html"?>
+
+
+<body>
+
+</body>
+
+<?php include_once "parts/footer.html"?>
+
+</html>
